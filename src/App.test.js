90fd6b9e2b4269441bbd80c9fd43f62c232d81c6
Nguyễn Hữu Nghĩa <nguyenhuunghia152@gmail.com>
2020-09-16 11:52:10 +0700

    test fail CI : test circleci

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/React docker demo!/i);
+  const linkElement = getByText(/React docker demo! test failed CI/i);
   expect(linkElement).toBeInTheDocument();
 });
