272e23b7b4c01aa10f73d1850a9ee5c1499b5508
Nguyễn Hữu Nghĩa <nguyenhuunghia152@gmail.com>
2020-09-16 11:54:47 +0700

    test fail CI : fix test bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React docker demo!</p>
+        <p>React docker demo final!</p>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/React docker demo! test failed CI/i);
+  const linkElement = getByText(/React docker demo final!/i);
   expect(linkElement).toBeInTheDocument();
 });
